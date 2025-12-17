0f2f88799661c6d3b38b0f0a21cc25703831165c
Naz <163540562+nazeeha-kb@users.noreply.github.com>
2025-12-17 22:50:04 +0300

    feat: added login, register, completed routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Layout from './components/Layout.jsx'
-import App from './App.jsx'
-import Dashboard from './pages/Dashboard/Dashboard.jsx'
-import Login from './pages/Login/Login.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Layout from "./components/Layout.jsx";
+import App from "./App.jsx";
+import Dashboard from "./pages/Dashboard/Dashboard.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Register from "./pages/Register/Register.jsx";
+import Tasks from "./pages/Tasks/Tasks.jsx";
+import Completed from "./pages/Completed/Completed.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
-  {path:'/', element:<Layout/>,
+  {
+    path: "/",
+    element: <Layout />,
     children: [
-      {index: true, element: <App/>},
-      {path:'/dashboard', element:<Dashboard/>},
-      {path:'/login', element:<Login/>},
-    ]
+      { index: true, element: <App /> },
+      { path: "/dashboard", element: <Dashboard /> },
+      { path: "/register", element: <Register /> },
+      { path: "/login", element: <Login /> },
+      { path: "/tasks", element: <Tasks /> },
+      { path: "/completed", element: <Completed /> },
+    ],
   },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>
-)
+);
