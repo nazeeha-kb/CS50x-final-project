3ccd2ee6ec19973ceb95a8a2793ef2b25db1f574
Naz <163540562+nazeeha-kb@users.noreply.github.com>
2025-12-17 11:29:57 +0300

    feat: update feature section content and UI

--- client/src/pages/Home/Features.jsx
+++ client/src/pages/Home/Features.jsx
@@ -3,22 +3,34 @@ import React from "react";
 const Features = () => {
   return (
     <section className="w-screen px-6 pb-30 pt-20 flex flex-col justify-center items-center">
-      <h2 className="font-semibold text-4xl pb-16 text-center">3 Steps to Clarity</h2>
+      <h2 className="font-semibold text-4xl pb-18 text-center">
+        3 Steps to Clarity
+      </h2>
       <ol className="flex flex-col lg:w-[60%] md:w-[68%] lg:gap-12 gap-10">
         <li className="feature">
           <span className="bg-green-400">1</span>
-          <p>Add top 3 tasks, select urgency, energy and time on bar</p>
+          <div>
+            <h3 className="font-bold">Capture Everything</h3>
+            <p>
+              Add all your tasks and set their urgency, required energy, and time.
+            </p>
+          </div>
         </li>
         <li className="feature">
           <span className="bg-green-600 text-white">2</span>
+          <div>
+            <h3 className="font-bold">Smart Prioritization</h3>
           <p>
-            The taks will be sorted accroding to the input you gave, choices are
-            made
+            The app analyzes your inputs and selects your top 3 tasks.
           </p>
+          </div>
         </li>
         <li className="feature">
           <span className="bg-green-400">3</span>
-          <p>Complete your tasks in order</p>
+          <div>
+          <h3 className="font-bold">Get It Done</h3>
+          <p>Complete your top 3 tasks, one at a time.</p>
+          </div>
         </li>
       </ol>
     </section>
