abd09880ef85adad241cb1f3e21d826a751d8c89
Naz <163540562+nazeeha-kb@users.noreply.github.com>
2025-12-17 22:43:50 +0300

    feat(layout): add flex layout for non-home pages

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
 export default function Layout() {
+
+  const location = useLocation();
+  const isHome = location.pathname === '/';
+
   return (
     <>
       <header>
         <Navbar />
       </header>
 
-      <main className="min-h-[79vh]">
+      <main className={`min-h-[79vh]  ${ isHome? "": "flex justify-center items-center"}`}>
         <Outlet />
       </main>
 
