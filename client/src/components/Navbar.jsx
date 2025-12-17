dcdbb7451c78c10508c5be1987339e7cf2fa8d17
Naz <163540562+nazeeha-kb@users.noreply.github.com>
2025-12-17 22:41:47 +0300

    feat: Added Sign Up button to Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,10 +9,21 @@ const Navbar = () => {
       <Link to="/" className="cursor-pointer">
         <img src={logo} alt="app logo" className="w-21 h-21" />
       </Link>
-      <Link to="/login" className="h-fit w-fit outline-1 px-5 py-3 mr-2 rounded-xl bg-slate-green text-white flex gap-2 items-center cursor-pointer font-semibold hover:bg-green-950">
-        <span>Log In</span>{" "}
+      <div className="flex">
+        <Link
+          to="/login"
+          className="h-fit w-fit outline-1 px-5 py-3 mr-2 rounded-xl border-[0.4px] border-gray-100 flex gap-2 items-center cursor-pointer font-semibold hover:bg-green-100"
+        >
+          Log In
+        </Link>
+        <Link
+          to="/register"
+          className="h-fit w-fit outline-1 px-5 py-3 mr-2 rounded-xl text-white flex gap-2 items-center bg-slate-green cursor-pointer font-semibold hover:bg-green-950"
+        >
+          <span>Sign Up</span>{" "}
           <FiLogIn className="text-xl" strokeWidth={2.5} />
-      </Link>
+        </Link>
+      </div>
     </nav>
   );
 };
