3e63fafad289182b8c5eb60e0004cc17c08d5fae
Naz <163540562+nazeeha-kb@users.noreply.github.com>
2025-12-29 20:09:37 +0300

    Add user authentication with Flask sessions and SQL database
    - Implement signup, login, and logout endpoints
    - Store users in SQLite database
    - Configure Flask-Session for session management

--- server/server.py
+++ server/server.py
@@ -1,40 +1,108 @@
-from flask import Flask,jsonify,request
-from flask_cors import CORS
-
+# For handling file path
+import os
 import sqlite3
+
+from flask import Flask,jsonify,request, session
+from flask_cors import CORS
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 
 # create app instance - configure app
 app = Flask(__name__)
-# enabling CORS to allow requests
-CORS(app)
 
-DB_path = f"C:\repos\CS50x-final-project\server\mindmint.db"
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
-# Database Setup
- 
-def init_db():   
+# enabling CORS to allow request
+CORS(app, origins=["http://localhost:5173"],  supports_credentials=True)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_path = os.path.join(BASE_DIR, "mindmint.db")
+
+# We don't need init_db since we created the db with CLI
+
+# Get the user for login from database
+def get_user(username):
     conn = sqlite3.connect(DB_path)
     cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, hash TEXT NOT NULL, cash NUMERIC NOT NULL DEFAULT 10000.00);
-        CREATE UNIQUE INDEX username ON users (username)
-''')
+    cursor.execute('SELECT * FROM users WHERE username = ?',(username,))
+    user = cursor.fetchall()
+    conn.close()
+    return user
+
+
+# Get usernames for validatoni from database
+def get_usernames(username):
+    conn = sqlite3.connect(DB_path)
+    cursor = conn.cursor()
+    cursor.execute('SELECT username FROM users WHERE username = ?',(username,))
+    user = cursor.fetchall()
+    conn.close()
+    return user
+
+
+# Add a user to database
+def add_user(username, hash):
+    conn = sqlite3.connect(DB_path)
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO users (username, hash) VALUES (?, ?)',(username, hash))
     conn.commit()
     conn.close()
 
+# TODO: Update user's password
 
+# Add tasks to DB
+def insert_tasks(taskName, priority_score, energy, urgency, time):
+    conn = sqlite3.connect(DB_path)
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO tasks (taskName, priority_score, energy, urgency, time, user_id) VALUES (?,?,?,?,?,1)', (taskName, priority_score, energy, urgency, time))
+    conn.commit()
+    conn.close()
+
+# Add a user to database
+def get_tasks():
+    conn = sqlite3.connect(DB_path)
+    cursor = conn.cursor()
+    cursor.execute('SELECT taskName, priority_score FROM tasks')
+    tasks = cursor.fetchall()
+    conn.close()
+    return tasks
+
+# Delete Tasks - when a session is over
+def delete_tasks(id):
+    conn = sqlite3.connect(DB_path)
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM tasks WHERE id = ?', (id))
+    conn.commit()
+    conn.close()
+
+    
+# TODO: Delete user
 
 # json - a list of dicts
 tasks = [
 ]
 
-# Tasks API route
-# If POST then read the tasks and append
-@app.route("/api/tasks",methods=["POST", "GET"])
-def dashboard():
+# 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/api/add_task",methods=["POST", "GET"])
+def add_task_route():
+
+    """Dashboard - Adding tasks"""
 
     global tasks
 
+# If POST then read the tasks and append
     if request.method == "POST":
 
         new_task = request.get_json()
@@ -47,32 +115,22 @@ def dashboard():
                 return jsonify ({"field":"taskName", "code":"REQUIRED"}),400
             
             else:
-                # Giving the tasks enumerating ids
-                # TODO: enumerate directly in SQL
-                # If the data list is empty, start with ID 1
-                if len(tasks) == 0:
-                    new_task["id"] = 1
-                else:
-                    # Get the last task's ID and add 1
-                    last_task = tasks[-1]  # the most recently added task
-                    new_task["id"] = last_task["id"] + 1
-                
                 # Calculating "priority_score" in order to calculate priority level.
                 new_task["priority_score"] = round((new_task["urgency"]*0.6) + (new_task["energy"]*0.2) + (new_task["time"]*0.2))
-
+                insert_tasks(new_task["taskName"], new_task["priority_score"], new_task["energy"], new_task["urgency"], new_task["time"])
                 tasks.append(new_task)
                 return jsonify ({"message": "Task added", "task":new_task}),201
             
         else:
             return jsonify({"error: Invalid JSON"}),400
     else:
-        return jsonify(tasks)
+        return jsonify(get_tasks())
     
 
 # Prioritize and return top 3 tasks
     
 @app.route("/api/prioritize",methods=["GET"])
-def prioritize_api():
+def prioritize_tasks():
     global tasks
      # Calculate the top 3 tasks from tasks array
     # We can just sort the array in order of priority_score and select top 3
@@ -103,8 +161,125 @@ def prioritize_api():
     return jsonify(top_tasks)
 
 
+@app.route("/api/logout", methods=["POST"])
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # send success message
+    return jsonify({"success": True}), 200
 
 
+
+@app.route("/api/signup", methods=["POST"])
+def singup():
+
+    """Sign in the user"""
+
+    # Getting the data from FE
+    data = request.get_json()
+    if not data:
+        return jsonify({"code": "INVALID_JSON"}), 400
+
+    # Form validation - return error message if field missing
+    
+    username = data.get("username")
+    password = data.get("password")
+    confirmation = data.get("confirmation")
+
+    if not username:
+        return jsonify({"field":"username", "code":"REQUIRED"}),400
+
+    if not password:
+        return jsonify({"field":"password", "code":"REQUIRED"}),400
+
+    if not confirmation:
+        return jsonify({"field":"confirmation", "code":"REQUIRED"}),400
+
+    # If passwords don't match return error
+    if password != confirmation:
+        return jsonify({"field":"password", "code":"INVALID"}),400
+
+    # If username is taken return apology
+    rows = get_usernames(username)
+
+    if len(rows) != 0:
+        return jsonify({"field":"username", "code":"TAKEN"}),400
+
+    # Generate hash password
+    hash = generate_password_hash(password)
+
+    # Insert user into the database
+    add_user(username, hash)
+
+    # Logging the user in
+    users = get_user(username)
+    # from the rows logging in the first user
+    session["user_id"] = users[0][0]
+
+    print("Sign in Successful!")
+    
+    # Send success message if logged in
+    return jsonify({"success": True}), 200
+
+
+@app.route("/api/login", methods=["POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Get the data from FE
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"code": "INVALID_JSON"}), 400
+    
+    # Extracting username
+    username = data.get("username")
+    password = data.get("password")
+
+    # Ensure username was submitted
+    if not username:
+        return jsonify({"field":"username", "code":"REQUIRED"}),400
+
+    # Ensure password was submitted
+    elif not password:
+        return jsonify({"field":"password", "code":"REQUIRED"}),400
+
+    # Query database for username
+    rows = get_user(username)
+
+    # Ensure username exists and password is correct
+    if len(rows) != 1 or not check_password_hash(
+        rows[0][2], password
+    ):
+        return jsonify({"field":"INVALID_CREDENTIALS", "code":"INVALID"}),400
+
+    # Remember which user has logged in
+    session["user_id"] = rows[0][0]
+
+    print("user_id", rows[0][0])
+
+    # Send success message if logged in
+    return jsonify({"success": True}), 200
+
+@app.route("/api/auth/status")
+def authStatus():
+    """Check if user is logged in"""
+
+    if "user_id" not in session:
+        print("logged out")
+        return jsonify({"logged_in": False}), 200
+    else:
+        print("logged in")
+        return jsonify({
+            "logged_in": True,
+        })
+       
 
 
 # Running app
