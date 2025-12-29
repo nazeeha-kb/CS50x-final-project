d25f8a23d96aa491d78478819ca565a6e3bac31d
Naz <163540562+nazeeha-kb@users.noreply.github.com>
2025-12-29 20:24:51 +0300

    Add frontend signup, login, and logout functionality
    - Implement Signup and Login pages with form handling
    - Connect forms to backend via Axios for authentication
    - Add logout functionality in Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,29 +1,58 @@
-import React from "react";
+import { use, useEffect, useState } from "react";
 import logo from "../assets/logo.png";
-import { FiLogIn } from "react-icons/fi";
+import { FiLogIn, FiLogOut } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import api from "../api";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const { user, setUser } = useAuth();
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    console.log("logging out");
+    api
+      .post("/logout")
+      .then((res) => {
+        if (res.data.success) {
+          setUser(null);
+          navigate("/");
+        }
+      })
+      .catch((err) => console.error(err.response?.data));
+  };
+
   return (
     <nav className="py-[0.2rem] px-[clamp(0.5rem,5vw,2rem)] border-b-[0.5px] border-gray-300 flex justify-between bg-green-50 items-center">
       <Link to="/" className="cursor-pointer">
         <img src={logo} alt="app logo" className="w-21 h-21" />
       </Link>
-      <div className="flex">
-        <Link
-          to="/login"
-          className="h-fit w-fit outline-1 px-5 py-3 mr-2 rounded-xl border-[0.4px] border-gray-100 flex gap-2 items-center cursor-pointer font-semibold hover:bg-green-100"
-        >
-          Log In
-        </Link>
-        <Link
-          to="/register"
+      {user ? (
+        <div
+          onClick={logOut}
           className="h-fit w-fit outline-1 px-5 py-3 mr-2 rounded-xl text-white flex gap-2 items-center bg-slate-green cursor-pointer font-semibold hover:bg-green-950"
         >
-          <span>Sign Up</span>{" "}
-          <FiLogIn className="text-xl" strokeWidth={2.5} />
-        </Link>
-      </div>
+          <span>Log Out</span>
+          <FiLogOut className="text-xl" strokeWidth={2.5} />
+        </div>
+      ) : (
+        <div className="flex">
+          <Link
+            to="/login"
+            className="h-fit w-fit outline-1 px-5 py-3 mr-2 rounded-xl border-[0.4px] border-gray-100 flex gap-2 items-center cursor-pointer font-semibold hover:bg-green-100"
+          >
+            Log In
+          </Link>
+          <Link
+            to="/signup"
+            className="h-fit w-fit outline-1 px-5 py-3 mr-2 rounded-xl text-white flex gap-2 items-center bg-slate-green cursor-pointer font-semibold hover:bg-green-950"
+          >
+            <span>Sign Up</span>{" "}
+            <FiLogIn className="text-xl" strokeWidth={2.5} />
+          </Link>
+        </div>
+      )}
     </nav>
   );
 };
--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -1,29 +1,24 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import api from "../../api";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext";
 
-const Login = () => {
-  const { setUser } = useAuth();
-  const navigate = useNavigate();
+const Register = () => {
+  const navigate = useNavigate()
   const [data, setData] = useState({
     username: "",
     password: "",
+    confirmation: "",
   });
+
   const handleSubmit = async (event) => {
+    // stops normal submission
     event.preventDefault();
     try {
-      const res = await api.post("/login", data);
+      const res = api.post("/signup", data);
       console.log("form submitted", res?.data);
-      if (res.data.success) {
-        // setting auth state globally
-        setUser(res.data);
-        // redirecting user
-        navigate("/dashboard");
-      }
+      if (res.data.success) navigate("/dashboard")
     } catch (err) {
-      console.error(err.response.data || err.message);
+      console.error(err?.response?.data);
     }
   };
 
@@ -36,20 +31,29 @@ const Login = () => {
         <div className="flex flex-col w-full md:gap-8 gap-6">
           <input
             type="text"
-            name="username"
             placeholder="username"
             value={data.username}
             onChange={(e) => setData({ ...data, username: e.target.value })}
+            name="username"
             required
             className="bg-gray-50 rounded-md px-4 lg:py-4 py-3 placeholder:text-xl"
           />
           <input
             type="text"
             name="password"
+            required
             placeholder="password"
             value={data.password}
             onChange={(e) => setData({ ...data, password: e.target.value })}
+            className="bg-gray-50 rounded-md px-4 lg:py-4 py-3 placeholder:text-xl"
+          />
+          <input
+            type="text"
+            name="confirmation"
             required
+            placeholder="confirm password"
+            value={data.confirmation}
+            onChange={(e) => setData({ ...data, confirmation: e.target.value })}
             className="bg-gray-50 rounded-md px-4 lg:py-4 py-3 placeholder:text-xl"
           />
         </div>
@@ -57,18 +61,17 @@ const Login = () => {
           type="submit"
           className="bg-dark-green hover:bg-green-900 cursor-pointer rounded-md lg:px-8 px-6 md:py-4 py-2.5 text-white text-xl"
         >
-          Login
+          Sign Up
         </button>
       </form>
       <p className="flex justify-center gap-2 pt-4.5">
-        <span className="text-gray-600">Don't have an account yet?</span>
-        <Link to="/signup" className="text-blue-700 cursor-pointer">
-          Sign Up
+        <span className="text-gray-600">Already have an account?</span>
+        <Link to="/login" className="text-blue-700 cursor-pointer">
+          Log In
         </Link>
       </p>
     </div>
   );
 };
 
-export default Login;
-3;
+export default Register;
