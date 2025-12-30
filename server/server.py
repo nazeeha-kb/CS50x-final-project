242cb3b5e2a5e2dc9ee2458b895167c6dab8e362
Naz <163540562+nazeeha-kb@users.noreply.github.com>
2025-12-30 16:46:30 +0300

    status update logic, improved prioritize tasks logic

--- server/server.py
+++ server/server.py
@@ -26,19 +26,23 @@ DB_path = os.path.join(BASE_DIR, "mindmint.db")
 # Get the user for login from database
 def get_user(username):
     conn = sqlite3.connect(DB_path)
+    conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM users WHERE username = ?',(username,))
-    user = cursor.fetchall()
+    rows = cursor.fetchall()
+    user = [dict(row) for row in rows]
     conn.close()
     return user
 
 
-# Get usernames for validatoni from database
+# Get usernames for validation from database
 def get_usernames(username):
     conn = sqlite3.connect(DB_path)
+    conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
     cursor.execute('SELECT username FROM users WHERE username = ?',(username,))
-    user = cursor.fetchall()
+    rows = cursor.fetchall()
+    user = [dict(row) for row in rows]
     conn.close()
     return user
 
@@ -53,23 +57,26 @@ def add_user(username, hash):
 
 # TODO: Update user's password
 
-# Add tasks to DB
-def insert_tasks(taskName, priority_score, energy, urgency, time):
+# Get tasks from DB
+def get_tasks(user_id):
     conn = sqlite3.connect(DB_path)
+    conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO tasks (taskName, priority_score, energy, urgency, time, user_id) VALUES (?,?,?,?,?,1)', (taskName, priority_score, energy, urgency, time))
-    conn.commit()
-    conn.close()
-
-# Add a user to database
-def get_tasks():
-    conn = sqlite3.connect(DB_path)
-    cursor = conn.cursor()
-    cursor.execute('SELECT taskName, priority_score FROM tasks')
-    tasks = cursor.fetchall()
+    cursor.execute('SELECT id, taskName, priority, status FROM tasks WHERE user_id = ?',(user_id,))
+    rows = cursor.fetchall()
+    # this returns an array of dict instead of tuples so we can safely do task.id, etc in fe
+    tasks = [dict(row) for row in rows]
     conn.close()
     return tasks
 
+# Update task's status
+def update_status(status,user_id,id):
+    conn = sqlite3.connect(DB_path)
+    cursor = conn.cursor()
+    cursor.execute('UPDATE tasks SET status = ? WHERE user_id = ? AND id = ?',(status,user_id,id))
+    conn.commit()
+    conn.close()
+    
 # Delete Tasks - when a session is over
 def delete_tasks(id):
     conn = sqlite3.connect(DB_path)
@@ -80,10 +87,6 @@ def delete_tasks(id):
 
     
 # TODO: Delete user
-
-# json - a list of dicts
-tasks = [
-]
 
 # 
 @app.after_request
@@ -100,8 +103,6 @@ def add_task_route():
 
     """Dashboard - Adding tasks"""
 
-    global tasks
-
 # If POST then read the tasks and append
     if request.method == "POST":
 
@@ -117,21 +118,34 @@ def add_task_route():
             else:
                 # Calculating "priority_score" in order to calculate priority level.
                 new_task["priority_score"] = round((new_task["urgency"]*0.6) + (new_task["energy"]*0.2) + (new_task["time"]*0.2))
-                insert_tasks(new_task["taskName"], new_task["priority_score"], new_task["energy"], new_task["urgency"], new_task["time"])
-                tasks.append(new_task)
+            
+                # intialize user's task list if doesn't exist
+                if "tasks" not in session:
+                    session["tasks"] = []
+                    new_task["id"] = 1
+
+                elif len(session["tasks"]) < 1:
+                    new_task["id"] = 1
+
+                # setting incremental id for tasks
+                else:
+                    new_task["id"] = session["tasks"][-1]["id"] + 1
+
+                # append task in the array
+                session["tasks"].append(new_task)
+            
                 return jsonify ({"message": "Task added", "task":new_task}),201
             
         else:
             return jsonify({"error: Invalid JSON"}),400
     else:
-        return jsonify(get_tasks())
-    
+        # return jsonify(get_tasks())
+        return jsonify(session["tasks"])
 
 # Prioritize and return top 3 tasks
     
-@app.route("/api/prioritize",methods=["GET"])
+@app.route("/api/prioritize",methods=["POST","GET"])
 def prioritize_tasks():
-    global tasks
      # Calculate the top 3 tasks from tasks array
     # We can just sort the array in order of priority_score and select top 3
 
@@ -140,11 +154,12 @@ def prioritize_tasks():
         return task["priority_score"]
 
     #sorting with python's sort
-    sorted_tasks = sorted(tasks, key=get_priority, reverse=True)
+    sorted_tasks = sorted(session["tasks"], key=get_priority, reverse=True)
 
     top_tasks = []
     priority = ["First","Second", "Third"] 
 
+                
     # store top 3 tasks in top_tasks array
     if len(sorted_tasks) > 3:
         for i in range(3):
@@ -152,13 +167,67 @@ def prioritize_tasks():
     else:
         top_tasks = sorted_tasks
 
+    # Clear the tasks array
+    session["tasks"] = []
+
     # for each value in top_tasks set priority
-    # run a loop for lentght of top tasks and take the i from priority and set respectively
+    # run loop len(top_tasks) times, taking i from priority list, and set priority for each task.
     for i in range(len(top_tasks)):
         top_tasks[i]["priority"] = priority[i]
+
+    # Inserting the top tasks into db 
+    conn = sqlite3.connect(DB_path)
+    cursor = conn.cursor()
+    # TODO: Delete previous tasks when all task status = done 
+    cursor.execute("DELETE FROM tasks WHERE user_id = ?", (session["user_id"],))
+    for task in top_tasks:
+            cursor.execute(
+        'INSERT INTO tasks (taskName, priority_score, energy, urgency, time, user_id, priority) VALUES (?,?,?,?,?,?,?)',
+        (
+            task["taskName"],
+            task["priority_score"],
+            task["energy"],
+            task["urgency"],
+            task["time"],
+            session["user_id"],
+            task["priority"]
+        )
+        )
     
-    # If GET then send the data to be displayed
+    for task in top_tasks:
+        if task["priority"] == "First":
+            print("setting priority of",task)
+            # first task's status is active
+            cursor.execute('UPDATE tasks SET status = ? WHERE user_id = ? AND priority = ?',("active",session["user_id"], "First"))
+
+    conn.commit()
+    conn.close()
+        
+    print(get_tasks(session["user_id"]))
+    
+    
+    return jsonify({"success":True}),200
+
+
+# Send top tasks to frontend
+@app.route("/api/tasks",methods=["GET"])
+def tasks():
+    # fetch tasks from db and set them to be top_tasks for persistance
+    top_tasks = []
+    top_tasks = get_tasks(session["user_id"])
+
+    # Send top tasks to frontend
     return jsonify(top_tasks)
+
+@app.route("/api/status_update/<int:id>", methods=["PUT"])
+def status_update(id):
+    """Update status of task"""
+    data = request.get_json()
+    status = data.get("status")
+    # update task in DB where task.id == id
+    update_status(status,session["user_id"],id)
+    update_status("active",session["user_id"],id+1)
+    return jsonify({"success":True}),200
 
 
 @app.route("/api/logout", methods=["POST"])
@@ -217,7 +286,7 @@ def singup():
     # Logging the user in
     users = get_user(username)
     # from the rows logging in the first user
-    session["user_id"] = users[0][0]
+    session["user_id"] = users[0]["id"]
 
     print("Sign in Successful!")
     
@@ -255,14 +324,12 @@ def login():
 
     # Ensure username exists and password is correct
     if len(rows) != 1 or not check_password_hash(
-        rows[0][2], password
+        rows[0]["hash"], password
     ):
         return jsonify({"field":"INVALID_CREDENTIALS", "code":"INVALID"}),400
 
     # Remember which user has logged in
-    session["user_id"] = rows[0][0]
-
-    print("user_id", rows[0][0])
+    session["user_id"] = rows[0]["id"]
 
     # Send success message if logged in
     return jsonify({"success": True}), 200
